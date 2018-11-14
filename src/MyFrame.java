3fbc3520d34bb7046444e41219f6940fa69ebb33
Luke Ward <luke.e.ward-1@ou.edu>
2018-11-14 14:27:56 -0600

    made a line

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,7 +4,7 @@ public class MyFrame extends JFrame
 {
     private final int WIDTH = 300;
     private final int HEIGHT = 400;
-    private MyPanel panel;
+    private static MyPanel panel;
     
     public MyFrame(String title)
     {
@@ -15,10 +15,19 @@ public class MyFrame extends JFrame
         setVisible(true);
         
     }
+    
+    public void paintLine()
+    {
+        panel = new LineDemoPanel();
+        add(panel);
+        setVisible(true);
+    }
 
     public static void main(String[] args)
     {
         MyFrame frame = new MyFrame("Hello");
+        frame.paintLine();
+        
     }
 
 }
